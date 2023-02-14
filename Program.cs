1b0f938b9b435c073bfb0bf4d78518160d2cabb9
Dan Biscup <dabiscup@microsoft.com>
2023-02-14 14:28:08 -0600

    trainer

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.IO;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        
+        string strTag                                                    = args[0];
+        string strConnectionString                                       = args[1];
+        string strContainer                                              = args[2];
+        string strProject                                                = args[3];
+        string strKey                                                    = args[4];
+        string strEndPt                                                  = args[5];
+
+        Console.WriteLine("Open computer vision");
+        CustomVisionTrainingClient trainingApi                          = new CustomVisionTrainingClient(new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.ApiKeyServiceClientCredentials(strKey))
+        {
+            Endpoint                                                    = strEndPt
+        };
+
+        Console.WriteLine("Find or create project: " + strProject);
+        Project p                                                       = null;
+        foreach(Project vp in trainingApi.GetProjects())
+        {
+            if(vp.Name == strProject)
+            {
+                p                                                       = vp;
+                break;
+            }
+        }
+
+        if (p == null)
+            p                                                           = trainingApi.CreateProject(strProject);
+
+        Console.WriteLine("Find or create tag: " + strTag);
+        var vTags                                                       = trainingApi.GetTags(p.Id);
+
+        Tag t                                                           = null;
+
+        foreach (Tag vt in vTags)
+        {
+            if (vt.Name == strTag)
+            {
+                t                                                       = vt;
+                break;
+            }
+        }
+
+        if (t == null)
+            t                                                           = trainingApi.CreateTag(p.Id, strTag);
+
+        Console.WriteLine("Train model with storage account container (" + strContainer + ")");
+        BlobServiceClient blobServiceClient                             = new BlobServiceClient(strConnectionString);
+        BlobContainerClient containerClient                             = blobServiceClient.GetBlobContainerClient(strContainer);
+        var blobs                                                       = containerClient.GetBlobs();
+        List<Guid> lstGuid                                              = new List<Guid>();
+        lstGuid.Add(t.Id);
+
+        try
+        {
+            foreach (var item in blobs)
+            {
+                Console.WriteLine("Train (" + strTag + ") with: " + item.Name);
+                var blobClient                                          = containerClient.GetBlobClient(item.Name);
+                trainingApi.CreateImagesFromData(p.Id, blobClient.OpenRead(), lstGuid);
+            }
+        }
+        catch (Exception e)
+        {
+            //  Block of code to handle errors
+            Console.WriteLine("Error", e);
+
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# azurecomputervisiontrain
+# Azure Computer Vision Trainer
+This example will take a blob storage connection (and container) and train a custom computer vision model.
+
+Below are the parameters it takes
+
+```c#
+
+        string strTag                       = args[0];
+        string strConnectionString          = args[1];
+        string strContainer                 = args[2];
+        string strProject                   = args[3];
+        string strKey                       = args[4];
+        string strEndPt                     = args[5];
+
+```
+
+
+<b>Tag</b> is what you want to tag the images as, like all these images are "cat" or "dog"
+
+<b>Connection String</b> string is to your Azure storage account
+
+<b>Container</b> is the storage container with your trainning images
+
+<b>Project</b> is the name of the computer vision project
+
+<b>Key</b> is the key to your computer vision training model
+
+<b>EndPt</b> is the end point to your computer vision training model
